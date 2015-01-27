5f3d79e741875503c0fc64a9779f6134fabf415c
Andrew Hobden <andrew@hoverbear.org>
2015-01-26 22:24:53 -0800

    Update docs for 3b4354120b7d7df7e9fbd064b37f774b5fad9770
    Various match tree improvements

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['raft'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='raft/enum.NodeState.html' title='raft::NodeState'>NodeState</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='raft/struct.LeaderState.html' title='raft::LeaderState'>LeaderState</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
